6a205234134c3ceb9fb9453cd5296ce31046d413
Coder-Chandler <ysiwgtus@gmail.com>
2017-07-15 22:43:46 +0800

    Scrapy Spider

--- scrapyspider/scrapyspider/spiders/lagoujob.py
+++ scrapyspider/scrapyspider/spiders/lagoujob.py
@@ -5,7 +5,7 @@ import scrapy
 class LagoujobSpider(scrapy.Spider):
     name = 'lagoujob'
     allowed_domains = ['www.lagou.com']
-    start_urls = ['http://www.lagou.com/']
+    start_urls = ['https://www.lagou.com/']
 
     def parse(self, response):
         pass
